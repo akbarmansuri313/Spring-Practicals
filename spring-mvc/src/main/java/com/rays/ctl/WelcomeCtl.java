3f2a4f1d590a205242f7c6c8ffa73655e7ab7660
akbarmansuri313 <ak9755530652@gmail.com>
2025-11-11 14:19:23 +0530

    done

---
+++ spring-mvc/src/main/java/com/rays/ctl/WelcomeCtl.java
@@ -0,0 +1,20 @@
+package com.rays.ctl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "WelcomeCtl")
+public class WelcomeCtl {
+	
+	@GetMapping
+	public String display(Model model) {
+		
+		model.addAttribute("message", "Welcome the View Jsp !!!!");
+		return "WelcomeView";		
+		
+	}
+
+}
